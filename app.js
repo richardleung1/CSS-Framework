3505533b0d81b92fb02e159b5fbf9d054aae21da
Richard Leung <michelleli@MMac.local>
2020-10-28 09:58:12 -0400

    Add js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-$('.carousel').carousel({
-    interval: 2000
-  });
+const buttonElement = document.createElement('button');
+const divElement = document.querySelector('form');
+buttonElement.classList.add('btn');
+buttonElement.classList.add('btn-primary');
+buttonElement.setAttribute('type', 'submit');
+buttonElement.textContent = 'Submit';
+divElement.appendChild(buttonElement);
